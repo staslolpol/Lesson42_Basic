e81f1b0fc495734e1229d31f15277a643031c915
staslolpol <staspriho9@gmail.com>
2026-02-18 17:10:34 +0300

    2

--- Lesson42/Task01.cpp
+++ Lesson42/Task01.cpp
@@ -5,6 +5,32 @@
 using namespace std;
 int main() {
 	
+	const int SIZE = 10;
+	int array[SIZE];
+
+	cout << "input your array elements: ";
+	for (int index = 0; index < SIZE; index++)
+	{
+		cin >> array[index];
+	}
+
+	int positive_sum = 0;
+	int negative_sum = 0;
+
+	for (int index = 0; index < SIZE; index++)
+	{
+		positive_sum += array[index] > 0 ? array[index] : 0;
+	}
+
+	for (int index = 0; index < SIZE; index++)
+	{
+		if (array[index] < 0) {
+			negative_sum += array[index];
+		}
+	}
+
+	cout << "Sum of positive element is " << positive_sum << '\n';
+	cout << "Sum of negative element is " << negative_sum << '\n';
 
 
 	return 0;
---
+++ Task02/main.cpp
@@ -0,0 +1,8 @@
+#include "logic.h"
+
+int main() {
+	//...
+
+
+	return 0;
+}
